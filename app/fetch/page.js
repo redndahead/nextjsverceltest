e2b5411ef644ce8b1a1b0dc2abeec2ad36b72bb6
Adam Moore <redndahead@gmail.com>
2023-09-04 15:57:24 -0700

    More debugging.

--- app/fetch/page.js
+++ app/fetch/page.js
@@ -1,11 +1,13 @@
 async function getData() {
+  console.log(process.env.AT_URL)
+  console.log(process.env.TOKEN)
   const res = await fetch(process.env.AT_URL, {
     headers: {Authentication: 'Bearer ' + process.env.TOKEN}
   })
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
-  console.log(res);
+  console.log(res)
  
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
@@ -17,6 +19,6 @@ async function getData() {
  
 export default async function Fetch() {
   const data = await getData()
-  console.log(data);
+  console.log(data)
   return <main></main>
 }
