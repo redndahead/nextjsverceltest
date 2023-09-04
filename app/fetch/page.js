3fa334a02062248924d5e46d9db04d68f63173ab
Adam Moore <redndahead@gmail.com>
2023-09-04 15:33:06 -0700

    Try calling rest api and debugging.

---
+++ app/fetch/page.js
@@ -0,0 +1,20 @@
+async function getData() {
+  const res = await fetch(process.env.AT_URL, {
+    headers: {Authentication: 'Bearer ' + process.env.TOKEN}
+  })
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+export default async function Page() {
+  const data = await getData()
+  console.log(data);
+  return <main></main>
+}
